7c7a2241f04b25057e6331499845d49b313a6714
sushma557 <60984376+sushma557@users.noreply.github.com>
2020-04-09 16:09:30 +0100

    Add files via upload

---
+++ sample.js
@@ -0,0 +1,34 @@
+function myfunction0(){
+	 document.getElementById("demo0").innerHTML="yes"
+}
+function myfunction(){
+  document.getElementById("demo").innerHTML="five"
+  
+}
+function myfunction1(){
+	document.getElementById("demo1").innerHTML="four"
+}
+function myfunction2(){
+	document.getElementById("demo2").innerHTML="Duck and Rabbit"
+}
+function myfunction3(){
+	document.getElementById("demo3").innerHTML="No blackdots"
+}
+function myfunction4(){
+	document.getElementById("demo4").innerHTML="Liar"
+}
+function riddlesfunction(){
+	document.getElementById("rid").innerHTML="Lunch and Dinner"
+}
+function riddlefunction1(){
+	document.getElementById("rid1").innerHTML="v"
+}
+function riddlefunction2(){
+	document.getElementById("rid2").innerHTML="Mushroom"
+}
+function riddlefunction3(){
+	document.getElementById("rid3").innerHTML="Post Office"
+}
+function riddlefunction4(){
+	document.getElementById("rid4").innerHTML="plate"
+}
